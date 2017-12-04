2fd5131842952b9072e8d2568c2fa5bd40940e8a
Adria-F <adriaferrer2898@gmail.com>
2017-12-04 21:02:16 +0100

    updated atlas + added switch on settings menu

--- Motor2D/j1UIScene.cpp
+++ Motor2D/j1UIScene.cpp
@@ -28,6 +28,7 @@ bool j1UIScene::Start()
 	_TTF_Font* big_buttons_font = App->font->Load("fonts/BMYEONSUNG.ttf", 50);
 	_TTF_Font* mid_buttons_font = App->font->Load("fonts/BMYEONSUNG.ttf", 30);
 	SDL_Color yellow_color = { 229, 168, 61, 255 };
+	SDL_Color dark_yellow_color = { 146, 97, 45, 255 };
 	_TTF_Font* big_texts_font = App->font->Load("fonts/TCCEB.ttf", 55);
 	SDL_Color black_color = { 0, 0, 0, 255 };
 	_TTF_Font* mid_texts_font = App->font->Load("fonts/TCCEB.ttf", 36);
@@ -35,23 +36,18 @@ bool j1UIScene::Start()
 	SDL_Texture* big_window_tex = App->tex->Load("gui/big_parchment.png");
 	SDL_Texture* mid_window_tex = App->tex->Load("gui/medium_parchment.png");
 
-	menu* mainMenu = new menu(INGAME_MENU);
+	menu* mainMenu = new menu(MAIN_MENU);
 	{
 		//TITLE
 		UI_element* title_img = App->gui->createImageFromAtlas(179 * App->gui->UI_scale, 92 * App->gui->UI_scale, { 0, 0, 664,147 }, this);
 
 		//NEW GAME
 		UI_element* new_game = App->gui->createButton(372 * App->gui->UI_scale, 341 * App->gui->UI_scale, NULL, { 0,148,281,111 }, { 281,148,281,111 }, { 562,148,281,111 }, this);
-		new_game->function = PAUSE;
 		new_game->dragable = true;
 
 		UI_element* new_text = App->gui->createText("NEW GAME", 200, 200, big_buttons_font, yellow_color);
 		new_text->setOutlined(true);
 		new_game->appendChildAtCenter(new_text);
-
-		//temp
-		/*Button* sliderButton = App->gui->createButton(0, 0, NULL, { 852, 189, 35, 42 }, { 852, 189, 35, 42 }, { 852, 189, 35, 42 }, this);
-		UI_element* slider = App->gui->createSlider(50, 200, NULL, { 0, 259, 638, 31 }, { 0, 290, 638, 31 }, sliderButton, big_buttons_font, yellow_color, 0, 168, 50);*/
 
 		//CONTINUE GAME
 		UI_element* continue_game = App->gui->createButton(372 * App->gui->UI_scale, 469 * App->gui->UI_scale, NULL, { 0,148,281,111 }, { 281,148,281,111 }, { 562,148,281,111 }, this);
@@ -66,7 +62,7 @@ bool j1UIScene::Start()
 		exit_game->setDragable(true, true);
 
 		UI_element* exit_text = App->gui->createText("EXIT", 200, 200, big_buttons_font, yellow_color);
-		exit_text->setDragable(true, true);
+		exit_text->setOutlined(true);
 		exit_game->appendChildAtCenter(exit_text);
 
 		//CREDITS
@@ -85,10 +81,6 @@ bool j1UIScene::Start()
 		settings_text->setOutlined(true);
 		settings->appendChildAtCenter(settings_text);
 
-
-		/*new_text = App->gui->createText("IN GAME", 0, 0, big_buttons_font, yellow_color);
-		new_text->setOutlined(true);*/
-
 		mainMenu->elements.add(title_img);
 		mainMenu->elements.add(new_game);
 		mainMenu->elements.add(new_text);
@@ -100,15 +92,13 @@ bool j1UIScene::Start()
 		mainMenu->elements.add(credits_text);
 		mainMenu->elements.add(settings);
 		mainMenu->elements.add(settings_text);
-		/*mainMenu->elements.add(slider);
-		mainMenu->elements.add(sliderButton);*/
 		menus.add(mainMenu);
 	}
 
 	menu* pauseMenu = new menu(PAUSE_MENU);
 	{
 		//PAUSE BUTTON
-		UI_element* pause_button = App->gui->createButton(947 * App->gui->UI_scale, 12 * App->gui->UI_scale, NULL, { 665,266,61,65 }, { 725,266,61,65 }, { 785,148,61,65 }, this);
+		UI_element* pause_button = App->gui->createButton(947 * App->gui->UI_scale, 12 * App->gui->UI_scale, NULL, { 666,266,60,63 }, { 726,266,60,63 }, { 786,266,60,63 }, this);
 		pause_button->setDragable(true, true);
 		pause_button->function = PAUSE;
 		
@@ -117,17 +107,17 @@ bool j1UIScene::Start()
 		pause_window->setDragable(true, true);
 
 		//SETTING CIRCLE BUTTON
-		UI_element* settings_button = App->gui->createButton(275 * App->gui->UI_scale, 414 * App->gui->UI_scale, NULL, { 876,341,120,123 }, { 876,465,120,123 }, { 876,589,120,123 }, this);
+		UI_element* settings_button = App->gui->createButton(275 * App->gui->UI_scale, 414 * App->gui->UI_scale, NULL, { 876,341,119,124 }, { 876,465,119,124 }, { 876,589,119,124 }, this);
 		settings_button->setDragable(true, true);
 		pause_window->appendChild(67 * App->gui->UI_scale, 233 * App->gui->UI_scale, settings_button);
 		
 		//PLAY CIRCLE BUTTON
-		UI_element* play_button = App->gui->createButton(439 * App->gui->UI_scale, 414 * App->gui->UI_scale, NULL, { 637,341,120,123 }, { 637,465,120,123 }, { 637,589,120,123 }, this);
+		UI_element* play_button = App->gui->createButton(439 * App->gui->UI_scale, 414 * App->gui->UI_scale, NULL, { 638,341,119,124 }, { 638,465,119,124 }, { 638,589,119,124 }, this);
 		play_button->setDragable(true, true);
 		pause_window->appendChild(231 * App->gui->UI_scale, 233 * App->gui->UI_scale, play_button);
 		
 		//RESTART CIRCLE BUTTON
-		UI_element* restart_button = App->gui->createButton(606 * App->gui->UI_scale, 414 * App->gui->UI_scale, NULL, { 757,341,120,123 }, { 757,465,120,123 }, { 757,589,120,123 }, this);
+		UI_element* restart_button = App->gui->createButton(606 * App->gui->UI_scale, 414 * App->gui->UI_scale, NULL, { 757,341,119,124 }, { 757,465,119,124 }, { 757,589,119,124 }, this);
 		restart_button->setDragable(true, true);
 		pause_window->appendChild(398 * App->gui->UI_scale, 233 * App->gui->UI_scale, restart_button);
 		
@@ -135,9 +125,6 @@ bool j1UIScene::Start()
 		UI_element* slider = App->gui->createImageFromAtlas(248 * App->gui->UI_scale, 310 * App->gui->UI_scale, { 0, 321, 504, 53 }, this);
 		slider->setDragable(true, true);
 		pause_window->appendChild(40 * App->gui->UI_scale, 129 * App->gui->UI_scale, slider);
-		
-		//test = App->gui->createText("PAUSE MENU", 0, 0, big_buttons_font, yellow_color);
-		//test->setOutlined(true);
 
 		pauseMenu->elements.add(pause_button);
 		pauseMenu->elements.add(pause_window);
@@ -163,7 +150,7 @@ bool j1UIScene::Start()
 
 		//MUSIC SLIDER
 		Button* music_slider_butt = App->gui->createButton(0, 0, NULL, { 852, 189, 35, 42 }, { 852, 189, 35, 42 }, { 852, 189, 35, 42 }, this);
-		UI_element* music_slider = App->gui->createSlider(0, 0, NULL, { 0, 259, 638, 31 }, { 0, 290, 638, 31 }, music_slider_butt, mid_texts_font, yellow_color, 0, 168, 50);
+		UI_element* music_slider = App->gui->createSlider(0, 0, NULL, { 0, 259, 638, 31 }, { 0, 290, 638, 31 }, music_slider_butt, mid_texts_font, dark_yellow_color, 0, 168, 50);
 		settings_window->appendChild(221 * App->gui->UI_scale, 136 * App->gui->UI_scale, music_slider);
 		
 		//FX TXT
@@ -172,7 +159,7 @@ bool j1UIScene::Start()
 
 		//FX SLIDER
 		Button* fx_slider_butt = App->gui->createButton(0, 0, NULL, { 852, 189, 35, 42 }, { 852, 189, 35, 42 }, { 852, 189, 35, 42 }, this);
-		UI_element* fx_slider = App->gui->createSlider(0, 0, NULL, { 0, 259, 638, 31 }, { 0, 290, 638, 31 }, fx_slider_butt, mid_texts_font, yellow_color, 0, 168, 50);
+		UI_element* fx_slider = App->gui->createSlider(0, 0, NULL, { 0, 259, 638, 31 }, { 0, 290, 638, 31 }, fx_slider_butt, mid_texts_font, dark_yellow_color, 0, 168, 50);
 		settings_window->appendChild(221 * App->gui->UI_scale, 220 * App->gui->UI_scale, fx_slider);
 
 		//GRAPHICS TXT
@@ -184,11 +171,14 @@ bool j1UIScene::Start()
 		settings_window->appendChild(60 * App->gui->UI_scale, 373 * App->gui->UI_scale, fullscreen_txt);
 
 		//FULLSCREEN SWITCH
-
+		Button* switchB = App->gui->createSwitch(0, 0, NULL, { 507, 329, 58, 39 }, { 507, 368, 58, 39 }, { 571, 329, 58, 39 }, { 571, 368, 58, 39 }, this);
+		switchB->setDragable(true, true);
+		settings_window->appendChild(222 * App->gui->UI_scale, 372 * App->gui->UI_scale, switchB);
 
 		//APPLY
 		UI_element* apply_button = App->gui->createButton(439 * App->gui->UI_scale, 414 * App->gui->UI_scale, NULL, { 666,0,168,66 }, { 666,67,168,66 }, { 835,0,168,66 }, this);
 		settings_window->appendChild(248 * App->gui->UI_scale, 457 * App->gui->UI_scale, apply_button);
+		apply_button->setDragable(true, true);
 
 		UI_element* apply_text = App->gui->createText("APPLY", 200, 200, mid_texts_font, yellow_color);
 		apply_text->setOutlined(true);
@@ -197,6 +187,7 @@ bool j1UIScene::Start()
 		//CANCEL
 		UI_element* cancel_button = App->gui->createButton(0, 0, NULL, { 666,0,168,66 }, { 666,67,168,66 }, { 835,0,168,66 }, this);
 		settings_window->appendChild(473 * App->gui->UI_scale, 457 * App->gui->UI_scale, cancel_button);
+		cancel_button->setDragable(true, true);
 
 		UI_element* cancel_text = App->gui->createText("CANCEL", 200, 200, mid_texts_font, yellow_color);
 		cancel_text->setOutlined(true);
@@ -205,6 +196,7 @@ bool j1UIScene::Start()
 		//RESTORE
 		UI_element* restore_button = App->gui->createButton(0, 0, NULL, { 849,69,133,36 }, { 849,106,133,36 }, { 849,143,133,36 }, this);
 		settings_window->appendChild(729 * App->gui->UI_scale, 478 * App->gui->UI_scale, restore_button);
+		restore_button->setDragable(true, true);
 
 		UI_element* restore_text = App->gui->createText("Restore default", 200, 200, small_texts_font, yellow_color);
 		restore_text->setOutlined(true);
@@ -213,9 +205,13 @@ bool j1UIScene::Start()
 		settingsMenu->elements.add(settings_window);
 		settingsMenu->elements.add(music_slider_butt);
 		settingsMenu->elements.add(fx_slider_butt);
+		settingsMenu->elements.add(apply_button);
+		settingsMenu->elements.add(cancel_button);
+		settingsMenu->elements.add(restore_button);
+		settingsMenu->elements.add(switchB);
 		menus.add(settingsMenu);
 	}
-	current_menu = settingsMenu;
+	current_menu = mainMenu;
 
 	return true;
 }
@@ -228,7 +224,7 @@ bool j1UIScene::PreUpdate()
 bool j1UIScene::Update(float dt)
 {
 	if (App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN)
-		LoadMenu(INGAME_MENU);
+		LoadMenu(MAIN_MENU);
 	else if (App->input->GetKey(SDL_SCANCODE_2) == KEY_DOWN)
 		LoadMenu(PAUSE_MENU);
 	else if (App->input->GetKey(SDL_SCANCODE_3) == KEY_DOWN)
